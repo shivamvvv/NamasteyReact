261f6c6857221bab0eb9e50e9d09865e23b67931
shivamxo <shivam0aa@gmail.com>
2025-02-18 00:19:12 +0530

    Ep06 Exploring the world

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,30 +1,66 @@
-import resList from "../utils/mockdata";
 import { search_icon } from "../Assests/assets";
 import Restaurantcard from "./Restaurantcard";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import Shimmer from "./shimmer";
 const Body = () => {
-  //let restaurants = [...resList];
-  const [listofrestaurants, setListOfRestaurants] = useState([...resList]);
+  const [listofrestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9615398&lng=79.296468&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING%22);"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || []
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || []
+    );
+  };
+  if (listofrestaurants?.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="search">
-        <input placeholder="Order Something !"></input>
-        <img src={search_icon}></img>
+        <input
+          placeholder="Order Something !"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        ></input>
+        <img
+          src={search_icon}
+          onClick={() => {
+            const filteredRestaurant = listofrestaurants.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredRestaurant(filteredRestaurant);
+          }}
+        ></img>
       </div>
       <button
         className="filter-btn"
         onClick={() => {
           const filtertheRestaurants = listofrestaurants.filter(
-            (res) => res.info.avgRating > 4.3
+            (res) => res.info.avgRating > 4.0
           );
-          setListOfRestaurants(filtertheRestaurants);
+          setFilteredRestaurant(filtertheRestaurants);
         }}
       >
         Top Rated Restaurants
       </button>
       <div className="res-container">
-        {listofrestaurants.map((data) => (
+        {filteredRestaurant.map((data) => (
           <Restaurantcard key={data.info.id} resData={data} />
         ))}
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,47 @@
 import AssetPath from "../Assests/assets";
-
+import { useState } from "react";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img className="logo" src={AssetPath.Logo}></img>
-        </div>
-  
-        <ul className="nav-items">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-  
-        <ul className="nav-items">
-          <li>
-            <img src={AssetPath.shopping_cart}></img>
-          </li>
-          <li>Profile</li>
-        </ul>
-      </div>
-    );
+  const [btnname, setBtnname] = useState("Sign In");
+  const btnStyle = {
+    cursor: "pointer",
+    color: "white",
+    backgroundColor: "#e55427",
+    padding: "10px 16px",
+    border: "none",
+    fontWeight: "500",
+    fontSize: "1rem",
+    borderRadius: "1rem",
   };
-  export default Header
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={AssetPath.Logo}></img>
+      </div>
+
+      <ul className="nav-items">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <ul className="nav-items">
+        <li>
+          <img src={AssetPath.shopping_cart}></img>
+        </li>
+        <button
+          style={btnStyle}
+          className="signInOut"
+          onClick={() => {
+            btnname === "Sign In"
+              ? setBtnname("Sign Out")
+              : setBtnname("Sign In");
+          }}
+        >
+          {btnname}
+        </button>
+      </ul>
+    </div>
+  );
+};
+export default Header;
